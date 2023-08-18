b9a2548ab267ffb0d787802872601804e513c6f2
xiaolu <Saladbob@outlook.com>
2023-08-18 11:17:56 +0800

    测试

--- zhishu_tiantian.py
+++ zhishu_tiantian.py
@@ -528,7 +528,7 @@ if __name__ == "__main__":
     # 配置基本的日志输出格式
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
     get_data()
-    git_commands()
+    # git_commands()
     scheduler = BlockingScheduler(timezone="Asia/Shanghai")
     try:
         scheduler.add_job(getWorkday, 'cron', day_of_week='0-6', hour=14, minute=50)
